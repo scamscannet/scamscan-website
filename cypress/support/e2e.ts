700d875dcbbd7a3ab61ed553a8d61001096860dc
nm <n.mlinski@wut.de>
2023-05-16 14:11:05 +0200

    comment function

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -19,33 +19,33 @@ import "./commands";
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-import {
-  AppRouterContext,
-  AppRouterInstance,
-} from "next/dist/shared/lib/app-router-context";
+// import {
+//   AppRouterContext,
+//   AppRouterInstance,
+// } from "next/dist/shared/lib/app-router-context";
 
-const createRouter = (params: Partial<AppRouterInstance>) => ({
-  back: cy.spy().as("back"),
-  forward: cy.spy().as("forward"),
-  prefetch: cy.stub().as("prefetch").resolves(),
-  push: cy.spy().as("push"),
-  replace: cy.spy().as("replace"),
-  refresh: cy.spy().as("refresh"),
-  ...params,
-});
+// const createRouter = (params: Partial<AppRouterInstance>) => ({
+//   back: cy.spy().as("back"),
+//   forward: cy.spy().as("forward"),
+//   prefetch: cy.stub().as("prefetch").resolves(),
+//   push: cy.spy().as("push"),
+//   replace: cy.spy().as("replace"),
+//   refresh: cy.spy().as("refresh"),
+//   ...params,
+// });
 
-interface MockNextRouterProps extends Partial<AppRouterInstance> {
-  children: React.ReactNode;
-}
+// interface MockNextRouterProps extends Partial<AppRouterInstance> {
+//   children: React.ReactNode;
+// }
 
-const MockNextRouter = ({ children, ...props }: MockNextRouterProps) => {
-  const router = createRouter(props as AppRouterInstance);
+// const MockNextRouter = ({ children, ...props }: MockNextRouterProps) => {
+//   const router = createRouter(props as AppRouterInstance);
 
-  return (
-    <AppRouterContext.Provider value={router}>
-      {children}
-    </AppRouterContext.Provider>
-  );
-};
+//   return (
+//     <AppRouterContext.Provider value={router}>
+//       {children}
+//     </AppRouterContext.Provider>
+//   );
+// };
 
-export default MockNextRouter;
+// export default MockNextRouter;
